e80b3675cda8ecc19cc513164caff26af5382e8d
vahid_saberi <vahid_saberi@yahoo.com>
2022-08-13 21:05:56 +0430

    first commit

---
+++ ShibaPiano/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace ShibaPiano
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btExit = new System.Windows.Forms.Button();
+            this.btPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbSheets = new System.Windows.Forms.ComboBox();
+            this.chbEnterText = new System.Windows.Forms.CheckBox();
+            this.btSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numTimeout = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.txtNotes.Location = new System.Drawing.Point(0, 31);
+            this.txtNotes.Margin = new System.Windows.Forms.Padding(1);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtNotes.Size = new System.Drawing.Size(1026, 198);
+            this.txtNotes.TabIndex = 0;
+            this.txtNotes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNotes_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 259);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1025, 231);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btExit
+            // 
+            this.btExit.Location = new System.Drawing.Point(938, 496);
+            this.btExit.Name = "btExit";
+            this.btExit.Size = new System.Drawing.Size(75, 23);
+            this.btExit.TabIndex = 2;
+            this.btExit.Text = "Exit";
+            this.btExit.UseVisualStyleBackColor = true;
+            this.btExit.Click += new System.EventHandler(this.BtExit_Click);
+            // 
+            // btPlay
+            // 
+            this.btPlay.Location = new System.Drawing.Point(12, 233);
+            this.btPlay.Name = "btPlay";
+            this.btPlay.Size = new System.Drawing.Size(75, 23);
+            this.btPlay.TabIndex = 3;
+            this.btPlay.Text = "Play";
+            this.btPlay.UseVisualStyleBackColor = true;
+            this.btPlay.Click += new System.EventHandler(this.BtPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 503);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Code :";
+            // 
+            // lblCode
+            // 
+            this.lblCode.AutoSize = true;
+            this.lblCode.Location = new System.Drawing.Point(52, 503);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(0, 13);
+            this.lblCode.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sheets :";
+            // 
+            // cmbSheets
+            // 
+            this.cmbSheets.FormattingEnabled = true;
+            this.cmbSheets.Location = new System.Drawing.Point(61, 6);
+            this.cmbSheets.Name = "cmbSheets";
+            this.cmbSheets.Size = new System.Drawing.Size(349, 21);
+            this.cmbSheets.TabIndex = 8;
+            this.cmbSheets.SelectedIndexChanged += new System.EventHandler(this.CmbSheets_SelectedIndexChanged);
+            // 
+            // chbEnterText
+            // 
+            this.chbEnterText.AutoSize = true;
+            this.chbEnterText.Location = new System.Drawing.Point(93, 235);
+            this.chbEnterText.Name = "chbEnterText";
+            this.chbEnterText.Size = new System.Drawing.Size(113, 17);
+            this.chbEnterText.TabIndex = 9;
+            this.chbEnterText.Text = "Edit text in the box";
+            this.chbEnterText.UseVisualStyleBackColor = true;
+            this.chbEnterText.CheckedChanged += new System.EventHandler(this.ChbEnterText_CheckedChanged);
+            // 
+            // btSave
+            // 
+            this.btSave.Enabled = false;
+            this.btSave.Location = new System.Drawing.Point(212, 233);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 10;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            this.btSave.Click += new System.EventHandler(this.BtSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(430, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Timeout between sheets";
+            // 
+            // numTimeout
+            // 
+            this.numTimeout.Location = new System.Drawing.Point(555, 7);
+            this.numTimeout.Margin = new System.Windows.Forms.Padding(1);
+            this.numTimeout.Maximum = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            this.numTimeout.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numTimeout.Name = "numTimeout";
+            this.numTimeout.Size = new System.Drawing.Size(45, 20);
+            this.numTimeout.TabIndex = 12;
+            this.numTimeout.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(604, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "millisecond";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 523);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numTimeout);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.chbEnterText);
+            this.Controls.Add(this.cmbSheets);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btPlay);
+            this.Controls.Add(this.btExit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtNotes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Shiba Piano";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTimeout)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btExit;
+        private System.Windows.Forms.Button btPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbSheets;
+        private System.Windows.Forms.CheckBox chbEnterText;
+        private System.Windows.Forms.Button btSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numTimeout;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ShibaPiano/Form1.cs
@@ -0,0 +1,406 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using ShibaPiano.Services;
+//using NAudio.Lame;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShibaPiano
+{
+    public partial class Form1 : Form
+    {
+        UserActivityHook actHook;
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        int indexOfParens = 0;
+
+        string SheetsFile = Directory.GetCurrentDirectory() + "/Sheets/";
+
+        string InputDirName = Directory.GetCurrentDirectory() + "/Sounds/";
+        //string OutputDirName = Directory.GetCurrentDirectory() + "/Combine/";
+
+        List<int> KeyPress = new List<int>();
+        Dictionary<int, int> ChangesKey = new Dictionary<int, int>()
+        {
+            [48] = -1,  //0
+            [49] = 33,  //1
+            [50] = 64,  //2
+            [51] = -1,  //3
+            [52] = 36,  //4
+            [53] = 37,  //5
+            [54] = 94,  //6
+            [55] = -1,  //7
+            [56] = 42,  //8
+            [57] = 40,  //9
+
+            [65] = 97,  //A
+            [66] = 98,  //B
+            [67] = 99,  //C
+            [68] = 100,  //D
+            [69] = 101,  //E
+            [70] = 102,  //F
+            [71] = 103,  //G
+            [72] = 104,  //H
+            [73] = 105,  //I
+            [74] = 106,  //J
+            [75] = 107,  //K
+            [76] = 108,  //L
+            [77] = 109,  //M
+            [78] = 110,  //N
+            [79] = 111,  //O
+            [80] = 112,  //P
+            [81] = 113,  //Q
+            [82] = 114,  //R
+            [83] = 115,  //S
+            [84] = 116,  //T
+            [85] = 117,  //U
+            [86] = 118,  //V
+            [87] = 119,  //W
+            [88] = 120,  //X
+            [89] = 121,  //Y
+            [90] = 122,  //Z
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            actHook = new UserActivityHook(); // crate an instance
+
+            // hang on events
+            //actHook.OnMouseActivity += new MouseEventHandler(MouseMoved);
+            actHook.KeyDown += new KeyEventHandler(MyKeyDown);
+            actHook.KeyUp += new KeyEventHandler(MyKeyUp);
+            //actHook.KeyPress += new KeyPressEventHandler(MyKeyPress);
+
+            this.LoadSheets();
+        }
+
+        //public void MyKeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    lblCode.Text = ((int)e.KeyChar).ToString();
+        //}
+
+        public void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            var keyValue = e.KeyValue;
+            
+            if (KeyPress.Contains(keyValue))
+                return;
+
+            KeyPress.Add(keyValue);
+
+            var typeOfKey = "a";
+
+            if ((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+                typeOfKey = "b";
+
+            if (typeOfKey == "a")
+            {
+                switch (keyValue)
+                {
+                    case 96: keyValue = 48; break;
+                    case 97: keyValue = 49; break;
+                    case 98: keyValue = 50; break;
+                    case 99: keyValue = 51; break;
+                    case 100: keyValue = 52; break;
+                    case 101: keyValue = 53; break;
+                    case 102: keyValue = 54; break;
+                    case 103: keyValue = 55; break;
+                    case 104: keyValue = 56; break;
+                    case 105: keyValue = 57; break;
+                }
+            }
+            else if (typeOfKey == "b")
+            {
+                switch (keyValue)
+                {
+                    case 45: keyValue = 48; break;
+                    case 35: keyValue = 49; break;
+                    case 40: keyValue = 50; break;
+                    case 34: keyValue = 51; break;
+                    case 37: keyValue = 52; break;
+                    case 12: keyValue = 53; break;
+                    case 39: keyValue = 54; break;
+                    case 36: keyValue = 55; break;
+                    case 38: keyValue = 56; break;
+                    case 33: keyValue = 57; break;
+                }
+            }
+
+
+
+            var soundName = typeOfKey + keyValue;
+            lblCode.Text = keyValue.ToString();
+
+            var path = this.InputDirName + $"{soundName}.mp3";
+
+            var paths = new List<string>();
+            paths.Add(path);
+
+            this.PlaySound(paths);
+
+            //txtNotes.Text += " ";
+            //txtNotes.SelectionStart = txtNotes.Text.Length;
+            //txtNotes.SelectionLength = 0;
+        }
+
+        public void MyKeyUp(object sender, KeyEventArgs e)
+        {
+            if (KeyPress.Contains(e.KeyValue))
+            {
+                KeyPress.Remove(e.KeyValue);
+            }
+        }
+        //void keyboard_KeyBoardKeyPressed(object sender, EventArgs e)
+        //{
+        //    var temp = sender.GetHashCode();
+        //}
+
+        //void mouse_MouseMoved(object sender, EventArgs e)
+        //{
+        //    mouseTime.Content = FormatDateTime(DateTime.Now);
+        //}
+
+        private void BtExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtPlay_Click(object sender, EventArgs e)
+        {
+            if (this.btPlay.Text == "Play")
+            {
+                this.PlayNotes(this.cts.Token);
+                this.btPlay.Text = "Stop";
+            }
+            else if (this.btPlay.Text == "Stop")
+            {
+                this.cts.Cancel(true);
+                this.btPlay.Text = "Play";
+                this.cts = new CancellationTokenSource();
+            }
+        }
+
+        private void PlaySound(List<string> paths)
+        {
+            if (paths.Count == 0)
+                return;
+
+            var readers = new List<AudioFileReader>();
+
+            foreach (var path in paths)
+            {
+                if (!File.Exists(path))
+                    continue;
+
+                readers.Add(new AudioFileReader(path));
+            }
+
+            if (readers.Count == 0)
+                return;
+
+            var mixer = new MixingSampleProvider(readers.ToArray());
+            //WaveFileWriter.CreateWaveFile16("mixed.wav", mixer);
+            var wo = new WaveOutEvent();
+            wo.Init(mixer);
+            wo.Play();
+        }
+
+        public void PlayNotes(CancellationToken token)
+        {
+            var notes = txtNotes.Text;
+            txtNotes.Focus();
+
+            Task.Run(() =>
+            {
+                indexOfParens = 0;
+                var multiple = false;
+                var paths = new List<string>();
+                var checkNote = true;
+
+                foreach (char note in notes)
+                {
+                    // do some work
+                    if (!token.IsCancellationRequested)
+                    {
+
+                        if (note == '[')
+                        {
+                            multiple = true;
+                            indexOfParens++;
+                            continue;
+                        }
+
+                        if (note == ']')
+                        {
+                            multiple = false;
+                            checkNote = false;
+                        }
+
+                        if (note == ' ')
+                        {
+                            indexOfParens++;
+                            continue;
+                        }
+
+                        this.Invoke(new Action(() =>
+                        {
+                            txtNotes.SelectionStart = indexOfParens;
+                            txtNotes.SelectionLength = 1;
+                        }));
+
+                        if (checkNote)
+                        {
+                            var typeOfKeye = "a";
+                            var value = (int)note;
+
+                            if (ChangesKey.ContainsKey(value))
+                            {
+                                typeOfKeye = "b";
+                                //value = ChangesKey.FirstOrDefault(x => x.Value == value).Key;
+                                value = ChangesKey[value];
+
+                                if (value == -1)
+                                {
+                                    indexOfParens++;
+                                    continue;
+                                }
+                            }
+
+                            var soundName = typeOfKeye + value;
+                            var path = this.InputDirName + $"{soundName}.mp3";
+
+                            paths.Add(path);
+                        }
+
+                        if (multiple)
+                        {
+                            indexOfParens++;
+                            continue;
+                        }
+
+                        this.PlaySound(paths);
+
+                        paths = new List<string>();
+                        checkNote = true;
+
+                        Thread.Sleep((int)numTimeout.Value);
+
+                        indexOfParens++;
+                    }
+                }
+
+                this.Invoke(new Action(() =>
+                {
+                    this.btPlay.Text = "Play";
+                }));
+
+            }, token);
+        }
+
+        private void CmbSheets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((ComboBox)sender).SelectedIndex == 0)
+            {
+                txtNotes.Text = "";
+                return;
+            }
+
+            string selected = ((ComboBox)sender).GetItemText(((ComboBox)sender).SelectedItem);
+            var path = SheetsFile + selected + ".txt";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                String line = sr.ReadToEnd();
+                txtNotes.Text = line;
+            }
+        }
+
+        private void ChbEnterText_CheckedChanged(object sender, EventArgs e)
+        {
+            if (((CheckBox)sender).Checked)
+            {
+                txtNotes.Focus();
+                btSave.Enabled = true;
+            }
+            else
+            {
+                btSave.Enabled = false;
+            }
+        }
+
+        private void TxtNotes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!chbEnterText.Checked)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void BtSave_Click(object sender, EventArgs e)
+        {
+            //var saved = false;
+            if (string.IsNullOrWhiteSpace(txtNotes.Text))
+            {
+                MessageBox.Show("Please enter notes in text box!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var fileName = cmbSheets.Text;
+            if (string.IsNullOrWhiteSpace(fileName) || fileName == "- Select a sample music sheet to autoplay -")
+            {
+                MessageBox.Show("Please enter name for save this sheet into the sheets dropdown!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var path = SheetsFile + fileName + ".txt";
+            if (File.Exists(path))
+            {
+                DialogResult dr = MessageBox.Show($"This name has already been used! {Environment.NewLine}Do you want To replace?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                //if (dr == DialogResult.Yes)
+                //{
+                //    using (var writer = new StreamWriter(path, true))
+                //    {
+                //        writer.WriteLine(txtNotes.Text);
+                //        saved = true;
+                //    }
+                //}
+                //else
+                //{
+                //    return;
+                //}
+
+                if (dr == DialogResult.No)
+                {
+                    return;
+                }
+            }
+
+            //if(!saved)
+            File.WriteAllText(path, txtNotes.Text);
+
+            this.LoadSheets();
+            chbEnterText.Checked = false;
+        }
+
+        private void LoadSheets()
+        {
+            cmbSheets.Items.Clear();
+            var filePaths = Directory.GetFiles(SheetsFile, "*.txt");
+            cmbSheets.Items.Add("- Select a sample music sheet to autoplay -");
+            if (filePaths.Count() > 0)
+            {
+                foreach (var item in filePaths)
+                {
+                    cmbSheets.Items.Add(Path.GetFileName(item).Replace(".txt", ""));
+                }
+            }
+            cmbSheets.SelectedIndex = 0;
+        }
+    }
+}
